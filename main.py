120641560b4e22009429d764e249942aba273e9a
rcsalomao <39416973+rcsalomao@users.noreply.github.com>
2026-05-29 16:02:25 -0300

    feat: insights. WIP

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
+import os
+from subprocess import run
+from shutil import which
+
 
 ESTADO_CIVIL_MAP = {
     1: "CASADO",
@@ -44,7 +48,34 @@ def get_dataframes(filename):
     return df
 
 
-df = get_dataframes("./data/Base Varejo.csv")
+# Verificar a existência do dataset e descompactá-lo caso necessário
+dataset_csv = os.path.isfile("./data/input/Base Varejo.csv")
+dataset_zip = os.path.isfile("./data/input/Base Varejo.csv.zip")
+if not dataset_csv:
+    if not dataset_zip:
+        print("ERRO: Arquivo do dataset não encontrado!")
+        print(
+            "Por favor, mover 'Base Varejo.csv.zip' ou 'Base Varejo.csv' para './data/input/'"
+        )
+        exit(1)
+    if not which("unzip"):
+        print("ERRO: Comando 'unzip' não encontrado!")
+        print(
+            "Por favor, instale 'unzip' ou utilize um sistema operacional de verdade c:"
+        )
+        print(
+            "Caso não seja possível, faça a extração manual do arquivo 'Base Varejo.csv.zip' para a pasta './data/input/'"
+        )
+        exit(1)
+    run(
+        'unzip -qq "./data/input/Base Varejo.csv.zip" -d "./data/input/"',
+        shell=True,
+        check=True,
+    )
+
+
+# Ingestão dos dados e criação de relatório
+df = get_dataframes("./data/input/Base Varejo.csv")
 
 
 # Percebe-se que pelo relatório de ingestão
@@ -62,10 +93,13 @@ print(f"  Linhas: {df.shape[0]} | Colunas: {df.shape[1]}")
 print()
 
 
-# Substituição de valores string "NULL", "N/A" e "" pelo valor 'None':
+# Substituição de valores string "NULL", "N/A", "" e "#N/D" pelo valor 'None':
 df.replace({v: None for v in ["NULL", "N/A", "", "#N/D"]}, inplace=True)
 print("Após a substituição de valores string nulos")
 print_nulos_por_col(df)
+# O total de nulos presente no dataset é de 0.4% nas colunas de categoria e nome do produto
+# Entendo que este valor não seja significativo e, portanto,
+# é plenamente possível fazer uso destes campos
 
 
 # Padronização do nome dos produtos, com a remoção de espaços extras e capitalização
@@ -81,7 +115,7 @@ print_nulos_por_col(df)
 
 
 # Substituição dos valores inteiros do campo 'CL_EC' para correspondente
-# em string, segundo o mapeamento dado em 'ESTADO_CIVIL_MAP'
+# em string, segundo o mapeamento dado em 'ESTADO_CIVIL_MAP' para melhor compreensão
 df["CL_EC"] = df["CL_EC"].map(lambda x: ESTADO_CIVIL_MAP[x])
 
 
@@ -104,3 +138,31 @@ print(df.groupby(by="CL_FHL")["CL_GENERO"].value_counts(normalize=True) * 100)
 # com 4 filhos, cujo há uma presença mais exarcebada de clientes masculinos.
 # Poderia-se presumir que nestes casos é a esposa a responsável por cuidar
 # das crianças enquanto o esposo realiza as compras.
+print()
+
+
+# Um resumo de quantidades proporcionais de produtos vendidos dentre algumas categorias
+print("Proporção de compras por gênero:")
+print(df["CL_GENERO"].value_counts(normalize=True) * 100)
+print("Número de vendas por categoria:")
+print(df["PR_CAT"].value_counts(normalize=True) * 100)
+print("Número de vendas por mês ao longo do período:")
+print(
+    df["DATA"]
+    .dt.month.value_counts(sort=False, normalize=True)
+    .to_frame()
+    .sort_values(by="DATA")
+    * 100
+)
+# print(
+#     df["PR_CAT"].unique()
+# )  # ['BEBIDAS' 'HIGIENE' 'ALIMENTOS' 'LIMPEZA' 'ACESSORIOS' 'PET' None]
+print("Número de vendas por categoria de produtos:")
+print("Produtos de HIGIENE:")
+print(df.query("PR_CAT == 'HIGIENE'")["PR_NOME"].value_counts(normalize=True) * 100)
+print("Produtos de ALIMENTOS:")
+print(df.query("PR_CAT == 'ALIMENTOS'")["PR_NOME"].value_counts(normalize=True) * 100)
+print("Produtos de LIMPEZA:")
+print(df.query("PR_CAT == 'LIMPEZA'")["PR_NOME"].value_counts(normalize=True) * 100)
+print("Produtos para PET:")
+print(df.query("PR_CAT == 'PET'")["PR_NOME"].value_counts(normalize=True) * 100)
