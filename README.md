9cc4a5eda33a17c15b10c53d8b9975f5d5614348
rcsalomao <39416973+rcsalomao@users.noreply.github.com>
2026-05-30 03:15:19 -0300

    feat: README. WIP

--- README.md
+++ README.md
@@ -1,2 +1,196 @@
 # varejo_analysis
-Projeto de análise de dados de uma base de varejo. Projeto avaliativo para o curso do SCTEC.
+Neste repositório consta a análise de dados do dataset de varejo, como projeto avaliativo do curso de análise de dados do SCTEC.
+
+# Objetivo
+Trata-se da análise e considerações à cerca das questões presentes no documento de instrução `./briefing/Análise de Dados com Python [T2] - M1S07 - Mini-Projeto Avaliativo .pdf`.
+
+A primeira parte corresponde às rotinas de ingestão, tratamento e limpeza dos dados brutos resultando num dataset limpo e ideal para as posteriores análises.
+
+Na sequência, as questões propostas pelo documento de instrução são respondidas e discutidas. Os Insights são melhor detalhados em seção específica posteriormente.
+
+Por fim, realiza-se a escrita dos dados limpos por meio de rotinas específicas.
+
+# Requisitos
+Este repositório faz uso das seguintes bibliotecas:
+
+- pandas
+- matplotlib
+
+Por favor, instale-as previamente.
+
+## Uso
+O ponto de entrada do algoritmo para análise é o arquivo `./main.py`.
+Basta, portanto, executar o respectivo arquivo pelo interpretador à partir da pasta raiz do projeto.
+
+# Resultados
+A execução do script resulta na exposição dos resultados e considerações do projeto no stdout do terminal, além do arquivo `./data/output/base_varejo_clean.csv` que representa o dataset processado e limpo em formato `csv`. O script, quando possível, também gera o arquivo compactado em `zip`.
+
+Adicionalmente, o script `./main.py` também contém comentários e conclusões à cerca dos processos de limpeza e deduções.
+
+## Ingestão dos dados
+A ingestão dos dados brutos é realizada pela função `get_dataframes` por meio do método `pandas.read_csv(...)` da biblioteca `pandas`.
+Adicionalmente é gerado um relatório inicial sobre o estado do mesmo, descrevendo a quantidade de dados duplicados, nulos e tipos das colunas encontradas.
+
+O output resultante da ingestão dos dados é descrito à seguir:
+
+```
+>>> Leitura de Dados
+-> Leitura de dados completada com sucesso
+
+==================================================
+              RELATÓRIO: Base Varejo              
+==================================================
+Linhas: 830000 | Colunas: 14
+
+Nulos por coluna:
+  Unnamed: 10: 830000 (100.0%)
+  Unnamed: 11: 830000 (100.0%)
+  Unnamed: 12: 830000 (100.0%)
+  Unnamed: 13: 830000 (100.0%)
+
+Duplicatas: 96553 (11.63%)
+
+Tipos das colunas:
+DATA            object
+CO_ID            int64
+CL_ID            int64
+CL_GENERO       object
+CL_EC            int64
+CL_FHL           int64
+CL_SEG          object
+PR_ID            int64
+PR_CAT          object
+PR_NOME         object
+Unnamed: 10    float64
+Unnamed: 11    float64
+Unnamed: 12    float64
+Unnamed: 13    float64
+dtype: object
+```
+
+## Limpeza dos dados
+
+A limpeza dos dados brutos é realizada pela função `process_limpeza_df` logo após a ingestão dos mesmos.
+
+Num primeiro momento foi identificada a presença de 4 colunas completamente nulas (de ids 10, 11, 12, 13) que foram propriadamente removidas do dataframe.
+
+Na sequência foi realizada a remoção dos dados duplicados, já que anteriormente foi detectado a presença dos mesmos na proporção de 11,63% do total.
+Entende-se que esta proporção é suficientemente pequena para se realizar a remoção completa.
+O resultado no terminal é de:
+```
+-> Após remoção dos dados duplicados
+  Linhas: 733447 | Colunas: 10
+```
+
+Realiza-se também a substituição de possíveis valores nulos do tipo string ("NULL", "N/A", "", "#N/D"), resultando no output à seguir.
+```
+-> Após a substituição de valores string nulos
+Nulos por coluna:
+  PR_CAT: 3228 (0.4%)
+  PR_NOME: 3228 (0.4%)
+```
+O total de nulos presente no dataset é de 0.4% nas colunas de categoria e nome do produto.
+Entendo que este valor não seja significativo e, portanto, é plenamente possível fazer uso destes campos.
+
+Adicionalmente também é feita a padronização dos nomes dos produtos por meio do seguinte código:
+```python
+df["PR_NOME"] = df["PR_NOME"].str.strip().str.upper()
+```
+
+Também se faz necessário realizar a conversão das informações contidas na coluna 'DATA' para o tipo apropriado `datetime`.
+Os valores que resultam em erros de conversão são implícitamente convertidos para `None`.
+Segue o output resultante:
+```
+-> Após a conversão dos valores no campo 'DATA'
+Tipos das colunas:
+DATA         datetime64[ns]
+CO_ID                 int64
+CL_ID                 int64
+CL_GENERO            object
+CL_EC                 int64
+CL_FHL                int64
+CL_SEG               object
+PR_ID                 int64
+PR_CAT               object
+PR_NOME              object
+dtype: object
+Nulos por coluna:
+  PR_CAT: 3228 (0.4%)
+  PR_NOME: 3228 (0.4%)
+```
+
+Por fim, realiza-se também a padronização dos valores do campo `CL_EC`, convertendo o número inteiro na sua categoria de fato em texto.
+```python
+df["CL_EC"] = df["CL_EC"].map(lambda x: ESTADO_CIVIL_MAP[x])
+```
+Em que `ESTADO_CIVIL_MAP` é um mapeamento do inteiro para a sua respectiva categoria string.
+
+## Estatísticas descritivas
+As estatísticas descritivas são realizadas na função `estat_descr`.
+À seguir são apresentadas as estatísticas descritivas para os dados referentes ao número de filhos dos clientes do dataset:
+```
+>>> Estatísticas descritivas para o de número de filhos dos clientes:
+count    733447.000000
+mean          1.146049
+std           1.416917
+min           0.000000
+25%           0.000000
+50%           0.000000
+75%           2.000000
+max           4.000000
+Name: CL_FHL, dtype: float64
+moda: 0
+```
+
+## Análise por agrupamentos
+As análises por agrupamentos são realizadas pela função `explor_agrup`.
+Primeiramente verifica-se alguma disparidade de comportamento entre gêneros:
+```
+-> Valores proporcionais de produtos comprados por gênero
+CL_GENERO  PR_CAT    
+F          ALIMENTOS     52.601941
+           HIGIENE       18.837512
+           LIMPEZA       17.683691
+           BEBIDAS        5.191012
+           PET            3.889582
+           ACESSORIOS     1.796262
+M          ALIMENTOS     52.627016
+           HIGIENE       18.879548
+           LIMPEZA       17.541289
+           BEBIDAS        5.293518
+           PET            3.932655
+           ACESSORIOS     1.725973
+Name: proportion, dtype: float64
+```
+Percebe-se que de forma geral não há uma diferença nas proporções de consumo entre os gêneros.
+Ambos são muito similares.
+
+Adicionalmente também é realizada a análise da proporção de clientes com a respectiva quantidade de filhos:
+```
+-> Distinção de gênero dos clientes com a respectiva quantidade de filhos
+CL_FHL  CL_GENERO
+0       F            52.995953
+        M            47.004047
+1       F            55.341516
+        M            44.658484
+2       F            50.386543
+        M            49.613457
+3       F            55.408140
+        M            44.591860
+4       M            58.508467
+        F            41.491533
+Name: proportion, dtype: float64
+```
+Percebe-se neste caso que, de forma geral (casos de 0, 1, 2 e 3 filhos), as clientes do gênero feminino são mais predominantes.
+Entretanto há uma inversão significativa para o caso específico de clientes com 4 filhos, cujo há uma presença mais exarcebada de pessoas do gênero masculino.
+
+## Insights
+
+### foo
+### foo
+### foo
+### foo
+### foo
+
+## Conclusão
+
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import os
 from subprocess import run
 from shutil import which
@@ -26,7 +25,7 @@ def print_nulos_por_col(df):
 
 
 def get_dataframes(filename):
-    # Carregando os dados
+    # Ingestão dos dados e criação do relatório de entrada
     print(">>> Leitura de Dados")
     df = pd.read_csv(filename, sep=";", encoding="utf-8-sig")
     print("-> Leitura de dados completada com sucesso")
@@ -47,6 +46,168 @@ def get_dataframes(filename):
     relatorio_qualidade(df, "Base Varejo")
     print()
     return df
+
+
+def process_limpeza_df(df: pd.DataFrame):
+    print(">>> Limpeza do dataframe")
+
+    # Percebe-se que pelo relatório de ingestão
+    # o dataset possui colunas completamente vazias.
+    # No caso, seriam as colunas de índice 10, 11, 12 e 13.
+    # Deve-se proceder então para a remoção destas mesmas colunas:
+    df.drop(columns=df.columns[[10, 11, 12, 13]], inplace=True)
+
+    # Foram identificadas aprox. 11,63% de linhas duplicadas no dataset.
+    # Procede-se, portanto, para a remoção das mesmas:
+    df.drop_duplicates(keep="first", inplace=True)
+    print("-> Após remoção dos dados duplicados")
+    print(f"  Linhas: {df.shape[0]} | Colunas: {df.shape[1]}")
+    print()
+
+    # Substituição de valores string "NULL", "N/A", "" e "#N/D" pelo valor 'None':
+    df.replace({v: None for v in ["NULL", "N/A", "", "#N/D"]}, inplace=True)
+    print("-> Após a substituição de valores string nulos")
+    print_nulos_por_col(df)
+    # O total de nulos presente no dataset é de 0.4% nas colunas de categoria e nome do produto
+    # Entendo que este valor não seja significativo e, portanto,
+    # é plenamente possível fazer uso destes campos
+
+    # Padronização do nome dos produtos, com a remoção de espaços extras e capitalização
+    df["PR_NOME"] = df["PR_NOME"].str.strip().str.upper()
+
+    # Conversão do tipo dos valores no campo 'DATA' para datetime
+    df["DATA"] = pd.to_datetime(df["DATA"], dayfirst=True, errors="coerce")
+    print("-> Após a conversão dos valores no campo 'DATA'")
+    print("Tipos das colunas:")
+    print(df.dtypes)
+    print_nulos_por_col(df)
+
+    # Substituição dos valores inteiros do campo 'CL_EC' para correspondente
+    # em string, segundo o mapeamento dado em 'ESTADO_CIVIL_MAP' para melhor compreensão
+    df["CL_EC"] = df["CL_EC"].map(lambda x: ESTADO_CIVIL_MAP[x])
+
+
+def estat_descr(df):
+    print(">>> Estatísticas descritivas para o de número de filhos dos clientes:")
+    print(df["CL_FHL"].describe())
+    print(f"moda: {df['CL_FHL'].mode().values[0]}")
+    print()
+
+
+def explor_agrup(df):
+    print(">>> Exploração de padrões por agrupamento:")
+
+    print("-> Valores proporcionais de produtos comprados por gênero")
+    print(df.groupby(by="CL_GENERO")["PR_CAT"].value_counts(normalize=True) * 100)
+    # É possível perceber que, proporcionalmente, o perfil de compra
+    # em função do gênero do cliente se mantém inalterado.
+    print()
+
+    print("-> Distinção de gênero dos clientes com a respectiva quantidade de filhos")
+    print(df.groupby(by="CL_FHL")["CL_GENERO"].value_counts(normalize=True) * 100)
+    # Percebe-se neste caso que, de forma geral (casos de 0, 1, 2 e 3 filhos),
+    # as clientes do gênero feminino são mais predominantes.
+    # Entretanto há uma inversão significativa para o caso específico de clientes
+    # com 4 filhos, cujo há uma presença mais exarcebada de pessoas do gênero masculino.
+    print()
+
+
+def insights(df):
+    print(">>> Insights alcançados")
+
+    print("-> Proporção de compras por gênero")
+    print(df["CL_GENERO"].value_counts(normalize=True) * 100)
+    # Percebe-se que há uma maior presença de clientes
+    # do gênero feminino neste dataset.
+    print()
+
+    print("-> Número de vendas por categoria")
+    print(df["PR_CAT"].value_counts(normalize=True) * 100)
+    # Por meio desta tabela, pode-se notar que os
+    # produtos da categoria de ALIMENTOS são os
+    # mais vendidos, contribuindo com mais de 52% das
+    # vendas. Já os produtos da categoria ACESSORIOS são
+    # os produtos que a menor representação com menos de
+    # 2% de representação.
+    print()
+
+    print("-> Número total de vendas por ano e diferença percentual")
+    df_2019 = df.loc[df["DATA"].dt.year == 2019, :]
+    df_2020 = df.loc[df["DATA"].dt.year == 2020, :]
+    n_vendas_2019 = (
+        df_2019["DATA"]
+        .dt.month.value_counts(sort=False, normalize=False)
+        .to_frame()
+        .sort_values(by="DATA")
+    )
+    n_vendas_2020 = (
+        df_2020["DATA"]
+        .dt.month.value_counts(sort=False, normalize=False)
+        .to_frame()
+        .sort_values(by="DATA")
+    )
+    print(f"Total de vendas em 2019: {n_vendas_2019['count'].sum()}")
+    print(f"Total de vendas em 2020: {n_vendas_2020['count'].sum()}")
+    print(
+        f"Diferença percentual: {((n_vendas_2020.sum() - n_vendas_2019.sum()) / n_vendas_2019.sum() * 100).values[0]}"
+    )
+    # Nota-se que houve um aumento de quase 10% no volume de vendas
+    # entre os anos de 2019 e 2020.
+    print()
+
+    print("-> Número de vendas por mês durante os dois anos")
+    print("Ano de 2019:")
+    print("  Quantidade de vendas por mês:")
+    print(n_vendas_2019["count"])
+    print("  Estatísticas das vendas por mês:")
+    print(n_vendas_2019.describe())
+    dias_unicos = df_2019["DATA"].unique()
+    dias_por_mes = pd.DataFrame.from_dict(
+        Counter([d.month for d in dias_unicos]), orient="index", columns=["quantidade"]
+    )
+    print("  Quantidade de vendas por mês normalizada pela quantidade de dias:")
+    vendas_por_dia_mes = n_vendas_2019["count"] / dias_por_mes["quantidade"]
+    print(vendas_por_dia_mes)
+    print()
+    print("Ano de 2020:")
+    print("  Quantidade de vendas por mês:")
+    print(n_vendas_2020["count"])
+    print("  Estatísticas das vendas por mês:")
+    print(n_vendas_2020.describe())
+    dias_unicos = df_2020["DATA"].unique()
+    dias_por_mes = pd.DataFrame.from_dict(
+        Counter([d.month for d in dias_unicos]), orient="index", columns=["quantidade"]
+    )
+    print("  Quantidade de vendas por mês normalizada pela quantidade de dias:")
+    vendas_por_dia_mes = n_vendas_2020["count"] / dias_por_mes["quantidade"]
+    print(vendas_por_dia_mes)
+    # Do ano de 2019 para 2020, o número de vendas médio aumentou quase 10%
+    # com a redução do respectivo desvio padrão em 35%, demonstrando que
+    # a amplitude dos valores mensais se tornou menor enquanto os valores
+    # mensais resultaram maiores.
+    # Ao se analisar as vendas mensais normalizadas pelo número de dias,
+    # percebe-se que não há a presença de um claro padrão a longo dos
+    # anos e entre os anos.
+    print()
+
+    # print(
+    #     df["PR_CAT"].unique()
+    # )  # ['BEBIDAS' 'HIGIENE' 'ALIMENTOS' 'LIMPEZA' 'ACESSORIOS' 'PET' None]
+    print("-> Número de vendas por categoria de produtos")
+    print("Produtos de HIGIENE:")
+    print(df.query("PR_CAT == 'HIGIENE'")["PR_NOME"].value_counts(normalize=True) * 100)
+    print()
+    print("Produtos de ALIMENTOS:")
+    print(
+        df.query("PR_CAT == 'ALIMENTOS'")["PR_NOME"].value_counts(normalize=True) * 100
+    )
+    print()
+    print("Produtos de LIMPEZA:")
+    print(df.query("PR_CAT == 'LIMPEZA'")["PR_NOME"].value_counts(normalize=True) * 100)
+    print()
+    print("Produtos para PET:")
+    print(df.query("PR_CAT == 'PET'")["PR_NOME"].value_counts(normalize=True) * 100)
+    print()
 
 
 # Verificar a existência do dataset e descompactá-lo caso necessário
@@ -75,154 +236,12 @@ if not dataset_csv:
     )
 
 
-# Ingestão dos dados e criação de relatório
 df = get_dataframes("./data/input/Base Varejo.csv")
 
+process_limpeza_df(df)
 
-# Percebe-se que pelo relatório de ingestão
-# o dataset possui colunas completamente vazias.
-# No caso, seriam as colunas de índice 10, 11, 12 e 13.
-# Deve-se proceder então para a remoção destas mesmas colunas:
-df.drop(columns=df.columns[[10, 11, 12, 13]], inplace=True)
+estat_descr(df)
 
+explor_agrup(df)
 
-# Foram identificadas aprox. 11,63% de linhas duplicadas no dataset.
-# Procede-se, portanto, para a remoção das mesmas:
-df.drop_duplicates(keep="first", inplace=True)
-print("Após remoção dos dados duplicados:")
-print(f"  Linhas: {df.shape[0]} | Colunas: {df.shape[1]}")
-print()
-
-
-# Substituição de valores string "NULL", "N/A", "" e "#N/D" pelo valor 'None':
-df.replace({v: None for v in ["NULL", "N/A", "", "#N/D"]}, inplace=True)
-print("Após a substituição de valores string nulos")
-print_nulos_por_col(df)
-# O total de nulos presente no dataset é de 0.4% nas colunas de categoria e nome do produto
-# Entendo que este valor não seja significativo e, portanto,
-# é plenamente possível fazer uso destes campos
-
-
-# Padronização do nome dos produtos, com a remoção de espaços extras e capitalização
-df["PR_NOME"] = df["PR_NOME"].str.strip().str.upper()
-
-
-# Conversão do tipo dos valores no campo 'DATA' para datetime
-df["DATA"] = pd.to_datetime(df["DATA"], dayfirst=True, errors="coerce")
-print("Após a conversão dos valores no campo 'DATA'")
-print("Tipos das colunas:")
-print(df.dtypes)
-print_nulos_por_col(df)
-
-
-# Substituição dos valores inteiros do campo 'CL_EC' para correspondente
-# em string, segundo o mapeamento dado em 'ESTADO_CIVIL_MAP' para melhor compreensão
-df["CL_EC"] = df["CL_EC"].map(lambda x: ESTADO_CIVIL_MAP[x])
-
-
-# Estatísticas para os valores de 'CL_FHL'
-print("Estatísticas descritivas para o de número de filhos dos clientes:")
-print(df["CL_FHL"].describe())
-print(f"moda: {df['CL_FHL'].mode().values[0]}\n")
-
-
-print(">>> Exploração de padrões por agrupamento:")
-
-print("Valores proporcionais de produtos comprados por gênero:")
-print(df.groupby(by="CL_GENERO")["PR_CAT"].value_counts(normalize=True) * 100)
-# É possível perceber que, proporcionalmente, o perfil de compra
-# em função do gênero do cliente se mantém inalterado.
-
-print("Distinção de gênero dos clientes com a respectiva quantidade de filhos:")
-print(df.groupby(by="CL_FHL")["CL_GENERO"].value_counts(normalize=True) * 100)
-# Percebe-se neste caso que, de forma geral (casos de 0, 1, 2 e 3 filhos),
-# as clientes do sexo feminino são mais predominantes.
-# Entretanto há uma inversão significativa para o caso específico de um casal
-# com 4 filhos, cujo há uma presença mais exarcebada de clientes masculinos.
-# Poderia-se presumir que nestes casos é a esposa a responsável por cuidar
-# das crianças enquanto o esposo realiza as compras.
-print()
-
-
-print(">>> Insights alcançados:")
-
-print("Proporção de compras por gênero:")
-print(df["CL_GENERO"].value_counts(normalize=True) * 100)
-# Percebe-se que há uma maior presença de clientes
-# do gênero feminino neste dataset.
-
-print("Número de vendas por categoria:")
-print(df["PR_CAT"].value_counts(normalize=True) * 100)
-# Por meio desta tabela, pode-se notar que os
-# produtos da categoria de ALIMENTOS são os
-# mais vendidos, contribuindo com mais de 52% das
-# vendas. Já os produtos da categoria ACESSORIOS são
-# os produtos que a menor representação com menos de
-# 2% de representação.
-
-print("Número total de vendas por ano e diferença percentual:")
-df_2019 = df.loc[df["DATA"].dt.year == 2019, :]
-df_2020 = df.loc[df["DATA"].dt.year == 2020, :]
-n_vendas_2019 = (
-    df_2019["DATA"]
-    .dt.month.value_counts(sort=False, normalize=False)
-    .to_frame()
-    .sort_values(by="DATA")
-)
-n_vendas_2020 = (
-    df_2020["DATA"]
-    .dt.month.value_counts(sort=False, normalize=False)
-    .to_frame()
-    .sort_values(by="DATA")
-)
-print(f"Total de vendas em 2019: {n_vendas_2019['count'].sum()}")
-print(f"Total de vendas em 2020: {n_vendas_2020['count'].sum()}")
-print(
-    f"Diferença percentual: {((n_vendas_2020.sum() - n_vendas_2019.sum()) / n_vendas_2019.sum() * 100).values[0]}"
-)
-# Nota-se que houve um aumento de quase 10% no volume de vendas
-# entre os anos de 2019 e 2020.
-
-print("Número de vendas por mês durante os dois anos:")
-print("  Ano de 2019:")
-print("  Quantidade de vendas por mês:")
-print(n_vendas_2019["count"])
-print(n_vendas_2019.describe())
-dias_unicos = df_2019["DATA"].unique()
-dias_por_mes = pd.DataFrame.from_dict(
-    Counter([d.month for d in dias_unicos]), orient="index", columns=["quantidade"]
-)
-print("  Quantidade de vendas por mês normalizada pela quantidade de dias:")
-vendas_por_dia_mes = n_vendas_2019["count"] / dias_por_mes["quantidade"]
-print(vendas_por_dia_mes)
-print("  Ano de 2020:")
-print("  Quantidade de vendas por mês:")
-print(n_vendas_2020["count"])
-print(n_vendas_2020.describe())
-dias_unicos = df_2020["DATA"].unique()
-dias_por_mes = pd.DataFrame.from_dict(
-    Counter([d.month for d in dias_unicos]), orient="index", columns=["quantidade"]
-)
-print("  Quantidade de vendas por mês normalizada pela quantidade de dias:")
-vendas_por_dia_mes = n_vendas_2020["count"] / dias_por_mes["quantidade"]
-print(vendas_por_dia_mes)
-# Do ano de 2019 para 2020, o número de vendas médio aumentou quase 10%
-# com a redução do respectivo desvio padrão em 35%, demonstrando que
-# a amplitude dos valores mensais se tornou menor enquanto os valores
-# mensais resultaram maiores.
-# Ao se analisar as vendas mensais normalizadas pelo número de dias,
-# percebe-se que não há a presença de um claro padrão a longo dos
-# anos e entre os anos.
-
-# print(
-#     df["PR_CAT"].unique()
-# )  # ['BEBIDAS' 'HIGIENE' 'ALIMENTOS' 'LIMPEZA' 'ACESSORIOS' 'PET' None]
-print("Número de vendas por categoria de produtos:")
-print("Produtos de HIGIENE:")
-print(df.query("PR_CAT == 'HIGIENE'")["PR_NOME"].value_counts(normalize=True) * 100)
-print("Produtos de ALIMENTOS:")
-print(df.query("PR_CAT == 'ALIMENTOS'")["PR_NOME"].value_counts(normalize=True) * 100)
-print("Produtos de LIMPEZA:")
-print(df.query("PR_CAT == 'LIMPEZA'")["PR_NOME"].value_counts(normalize=True) * 100)
-print("Produtos para PET:")
-print(df.query("PR_CAT == 'PET'")["PR_NOME"].value_counts(normalize=True) * 100)
+insights(df)
